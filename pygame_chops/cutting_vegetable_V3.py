f62290b9c53a5850d89da53efaed4453bded5bfd
melissaschang <40808810+melissaschang@users.noreply.github.com>
2022-03-08 18:56:14 -0800

    comment organizaiton

--- pygame_chops/cutting_vegetable_V3.py
+++ pygame_chops/cutting_vegetable_V3.py
@@ -1,66 +1,60 @@
 '''
-Add comments'''
+In this program, press 's' to begin and '1' '2' or '3' to adjust the speed of the cutting motion. Keep in mind that this version
+is a lot faster for demonstration purposes but will be a lot slower in the game so we can have the user cutting for ~30 seconds'''
 
 import pygame
 import time
 import threading as th
 import keyboard
 
-speed=1
-keep_going = True
-
-
-windowSize=(1200,900)
-
-win=pygame.display.set_mode(windowSize)
-
-#set title on window
-pygame.display.set_caption("Chopping") 
+#variables
 speed = 3
-#look in same folder as script for images
-c1 =pygame.image.load('choppingcarrot/c1.png')
-c2 =pygame.image.load('choppingcarrot/c2.png')
-c3 =pygame.image.load('choppingcarrot/c3.png')
-c4 =pygame.image.load('choppingcarrot/c4.png')
-c5 =pygame.image.load('choppingcarrot/c5.png')
-c6 =pygame.image.load('choppingcarrot/c6.png')
-c7 =pygame.image.load('choppingcarrot/c7.png')
-c8 =pygame.image.load('choppingcarrot/c8.png')
-c9 =pygame.image.load('choppingcarrot/c9.png')
-c10 =pygame.image.load('choppingcarrot/c10.png')
-c11 =pygame.image.load('choppingcarrot/c11.png')
-c12 =pygame.image.load('choppingcarrot/c12.png')
-c13 =pygame.image.load('choppingcarrot/c13.png')
-c14 =pygame.image.load('choppingcarrot/c14.png')
-c15 =pygame.image.load('choppingcarrot/c15.png')
-c16 =pygame.image.load('choppingcarrot/c16.png')
-c17 =pygame.image.load('choppingcarrot/c17.png')
-c18 =pygame.image.load('choppingcarrot/c18.png')
-c19 =pygame.image.load('choppingcarrot/c19.png')
-c20 =pygame.image.load('choppingcarrot/c20.png')
-c21 =pygame.image.load('choppingcarrot/c21.png')
-c22 =pygame.image.load('choppingcarrot/c22.png')
-c23 =pygame.image.load('choppingcarrot/c23.png')
-c24 =pygame.image.load('choppingcarrot/c24.png')
-c25 =pygame.image.load('choppingcarrot/c25.png')
-c26 =pygame.image.load('choppingcarrot/c26.png')
-c27 =pygame.image.load('choppingcarrot/c27.png')
-#knife = pygame.transform.scale(knife, (250, 220))
-board=pygame.image.load('cuttingboard2.png')
-#board = pygame.transform.scale(board, (300, 320))
+windowSize=(1200,900)
+clock = pygame.time.Clock()
 
+#window
+win=pygame.display.set_mode(windowSize)
+pygame.display.set_caption("Chopping") 
+
+#font stuff
+green = (2, 100,64)
+black = (0,0,0)
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 40)
 
-transparent = (0, 0, 0, 0) # leaf.image.fill(transparent)
-clock = pygame.time.Clock()
-
-    
+#look in same folder as script for images
+c1 =pygame.image.load('images\choppingcarrot\c1.png')
+c2 =pygame.image.load('images\choppingcarrot\c2.png')
+c3 =pygame.image.load('images\choppingcarrot\c3.png')
+c4 =pygame.image.load('images\choppingcarrot\c4.png')
+c5 =pygame.image.load('images\choppingcarrot\c5.png')
+c6 =pygame.image.load('images\choppingcarrot\c6.png')
+c7 =pygame.image.load('images\choppingcarrot\c7.png')
+c8 =pygame.image.load('images\choppingcarrot\c8.png')
+c9 =pygame.image.load('images\choppingcarrot\c9.png')
+c10 =pygame.image.load('images\choppingcarrot\c10.png')
+c11 =pygame.image.load('images\choppingcarrot\c11.png')
+c12 =pygame.image.load('images\choppingcarrot\c12.png')
+c13 =pygame.image.load('images\choppingcarrot\c13.png')
+c14 =pygame.image.load('images\choppingcarrot\c14.png')
+c15 =pygame.image.load('images\choppingcarrot\c15.png')
+c16 =pygame.image.load('images\choppingcarrot\c16.png')
+c17 =pygame.image.load('images\choppingcarrot\c17.png')
+c18 =pygame.image.load('images\choppingcarrot\c18.png')
+c19 =pygame.image.load('images\choppingcarrot\c19.png')
+c20 =pygame.image.load('images\choppingcarrot\c20.png')
+c21 =pygame.image.load('images\choppingcarrot\c21.png')
+c22 =pygame.image.load('images\choppingcarrot\c22.png')
+c23 =pygame.image.load('images\choppingcarrot\c23.png')
+c24 =pygame.image.load('images\choppingcarrot\c24.png')
+c25 =pygame.image.load('images\choppingcarrot\c25.png')
+c26 =pygame.image.load('images\choppingcarrot\c26.png')
+c27 =pygame.image.load('images\choppingcarrot\c27.png')
+board=pygame.image.load('images\cuttingboard2.png')
 
 
 def key_capture_thread():
     global speed
-    global keep_going
     a = keyboard.read_key()
     if a== "1":
         speed=1
@@ -71,18 +65,26 @@ def key_capture_thread():
     th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
 
 
-
-def do_stuff():
+def chopping():
     th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
     i=0
     global speed
-        
+
+    #intialize progress bar 
+    pygame.draw.rect(win, black, pygame.Rect(299, 520, 500, 30),2 )
+    pygame.display.update() 
+
     for i in range(2,28):
         clock.tick(2*speed)        
         win.blit(board, (300, 110))
         var_name2 = "c"+str(i)
         #print(var_name2)
         win.blit(globals()[var_name2], (300, 110))
+
+        #increment progress bar
+        pygame.draw.rect(win, black, pygame.Rect(299, 520, 500, 30),2 )
+        x = round(i/27*10)-1
+        pygame.draw.rect(win, green, pygame.Rect(300+(50*x), 521, 48, 28) )
         pygame.display.update()
         
     print ("exit")
@@ -93,13 +95,25 @@ def main():
    # knifeInfo = pygame.Rect(0,0, 50, 50)  #x,y,width,height
     
     run=False
+
+    #fill background white
     blueBackground=(255, 255, 255) # red, green, blue tuple
     win.fill(blueBackground)
     
+    #draw instructions
     msg= myfont.render('Press \'s\' to start', False, (0,0,0))
     win.blit(msg, (200,50))
+
+    #draw the board
     win.blit(board, (300, 110))
+
+    #draw the carrot uncut
     win.blit(c1, (300, 110))
+
+    #draw progress bar outline
+    pygame.draw.rect(win, black, pygame.Rect(299, 520, 500, 30),2 )
+
+
     pygame.display.update()
             
     
@@ -114,11 +128,10 @@ def main():
 
         
         if keys_pressed[pygame.K_s]: #s to start
-            do_stuff()
+            chopping()
 
 
-                  
-                
+              
     pygame.quit()
 
 
--- pygame_chops/stir_v2.py
+++ pygame_chops/stir_v2.py
@@ -1,47 +1,57 @@
 import pygame
 import time
 import threading as th
-
 import keyboard
-#define size of window
+
+#variables
 windowSize=(1200,900)
-
-win=pygame.display.set_mode(windowSize)
-backgroundColor=(255, 255, 255) # red, green, blue tuple
-#set title on window
-pygame.display.set_caption("Stirring") 
 speed = 3
-
 clock = pygame.time.Clock()
 
-#look in same folder as script for images
-s1 =pygame.image.load('stir\s1.png')
-s2 =pygame.image.load('stir\s2.png')
-s3 =pygame.image.load('stir\s3.png')
-s4 =pygame.image.load('stir\s4.png')
-s5 =pygame.image.load('stir\s5.png')
-s6 =pygame.image.load('stir\s6.png')
-s7 =pygame.image.load('stir\s7.png')
-s8 =pygame.image.load('stir\s8.png')
-s9 =pygame.image.load('stir\s9.png')
-s10 =pygame.image.load('stir\s10.png')
-s11 =pygame.image.load('stir\s11.png')
-s12 =pygame.image.load('stir\s12.png')
-s13 =pygame.image.load('stir\s13.png')
-s14 =pygame.image.load('stir\s14.png')
-s15 =pygame.image.load('stir\s15.png')
-s16 =pygame.image.load('stir\s16.png')
-s17 =pygame.image.load('stir\s17.png')
-s18 =pygame.image.load('stir\s18.png')
-s19 =pygame.image.load('stir\s19.png')
-s20 =pygame.image.load('stir\s20.png')
-s21 =pygame.image.load('stir\s21.png')
-s22 =pygame.image.load('stir\s22.png')
-s23 =pygame.image.load('stir\s23.png')
+#setup screen
+win=pygame.display.set_mode(windowSize)
+backgroundColor=(255, 255, 255) # red, green, blue tuple
+pygame.display.set_caption("Stirring") 
 
 
+#colors
+green = (2, 100,64)
+black = (0,0,0)
+
+
+#text
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 40)
+good= myfont.render('Good job!', False, (0,0,0))
+great=myfont.render('Great job!', False, (0,0,0))
+ok=myfont.render('Ok job!', False, (0,0,0))
+
+
+#load all images
+s1 =pygame.image.load('images\stir\s1.png')
+s2 =pygame.image.load('images\stir\s2.png')
+s3 =pygame.image.load('images\stir\s3.png')
+s4 =pygame.image.load('images\stir\s4.png')
+s5 =pygame.image.load('images\stir\s5.png')
+s6 =pygame.image.load('images\stir\s6.png')
+s7 =pygame.image.load('images\stir\s7.png')
+s8 =pygame.image.load('images\stir\s8.png')
+s9 =pygame.image.load('images\stir\s9.png')
+s10 =pygame.image.load('images\stir\s10.png')
+s11 =pygame.image.load('images\stir\s11.png')
+s12 =pygame.image.load('images\stir\s12.png')
+s13 =pygame.image.load('images\stir\s13.png')
+s14 =pygame.image.load('images\stir\s14.png')
+s15 =pygame.image.load('images\stir\s15.png')
+s16 =pygame.image.load('images\stir\s16.png')
+s17 =pygame.image.load('images\stir\s17.png')
+s18 =pygame.image.load('images\stir\s18.png')
+s19 =pygame.image.load('images\stir\s19.png')
+s20 =pygame.image.load('images\stir\s20.png')
+s21 =pygame.image.load('images\stir\s21.png')
+s22 =pygame.image.load('images\stir\s22.png')
+s23 =pygame.image.load('images\stir\s23.png')
+
 
 
 
@@ -60,10 +70,16 @@ def key_capture_thread():
 
 
 
-def do_stuff():
+def stirring():
     th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
     i=0
     global speed
+
+
+     
+    
+    pygame.draw.rect(win, black, pygame.Rect(299, 520, 500, 30),2 )
+    pygame.display.update()
 
     for i in range(1,115):
         k=i
@@ -79,8 +95,30 @@ def do_stuff():
         
         clock.tick(2*speed)  
         var_name = "s"+str(k)
+
+        #redraw background
         win.fill(backgroundColor)
+
+        #draw next frame
         win.blit(globals()[var_name], (300, 110))
+
+        #redraw progress bar outline
+        pygame.draw.rect(win, black, pygame.Rect(299, 520, 500, 30),2 )
+
+        '''
+        if (speed==3):
+            win.blit(great, (200,50))
+        elif speed==2:
+            win.blit(good, (200,50))
+        else:
+            win.blit(ok, (200,50))
+        '''
+
+        #increment progress barS
+        x = round(i/115*10)
+        for t in range(0,x):
+            pygame.draw.rect(win, green, pygame.Rect(300+(50*t), 521, 48, 28) )
+
         pygame.display.update()
         
     print ("exit")
@@ -90,10 +128,19 @@ def do_stuff():
 def main():
 
     win.fill(backgroundColor)
+
     
     msg= myfont.render('Press \'s\' to start', False, (0,0,0))
+
+    #blit puts one image on another  
+    #display instructions 
     win.blit(msg, (200,50))
+
+    #draw soup
     win.blit(s1, (300, 110))
+
+    pygame.draw.rect(win, black, pygame.Rect(299, 520, 500, 30),2 )
+    #update pictures
     pygame.display.update()
     run= True
     while run:
@@ -102,10 +149,10 @@ def main():
                 print('User quit game')
                 run=False
         
-    
+        #if you press s, the screen begins stirring
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_s]: #s to start
-            do_stuff()
+            stirring()
                   
                 
     pygame.quit()
