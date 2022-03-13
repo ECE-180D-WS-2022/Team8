8967e5330ac4d6dab60a324312b2d130b3a55ee6
meredithtill <97260198+meredithtill@users.noreply.github.com>
2022-03-12 16:44:42 -0800

    new with square to calibrate

---
+++ outputsquarecal.py
@@ -0,0 +1,119 @@
+import time as t 
+import numpy as np
+import cv2
+#vision processing code
+cap = cv2.VideoCapture(2)
+init_cal = False
+x_c_1 = 0
+y_c_1 = 0
+x_c_2 = 0
+y_c_2 = 0
+lower_thresh_LED = np.array([159, 80, 200]) #RED LED
+upper_thresh_LED = np.array([180, 255, 255]) #RED LED
+counter = 0 
+x_pos  = 0
+
+def track_player(frame, lower_thresh_player, upper_thresh_player):
+    global x_pos
+    global prev_x
+    global prev_y
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Threshold the HSV image to get colors of interest
+    mask = cv2.inRange(hsv, lower_thresh_player, upper_thresh_player)
+    ret,thresh = cv2.threshold(mask,127,255,0)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    #from threshholding cv doc
+    th3 = cv2.adaptiveThreshold(mask,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 4000:
+            x,y,w,h = cv2.boundingRect(i)
+            #print(prev_x)
+            #print(x)
+            #if abs(prev_x - x) <= 150:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            x_pos = (x + int(w/2))*2
+
+def get_calibration_frames(frame):
+    global x_c_1
+    global x_c_2
+    global y_c_1
+    global y_c_2
+    global counter
+    x_c_1 = 200
+    x_c_2 = 400
+    y_c_1 = 250
+    y_c_2 = 495
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    cv2.rectangle(frame, (x_c_1, y_c_1), (x_c_2, y_c_2), (0, 255, 0), 3)
+
+def calibrate(frame, x_c_1, y_c_1, x_c_2, y_c_2):
+    global lower_thresh_player
+    global upper_thresh_player
+    global prev_x
+    global prev_y
+    global x_pos
+    cv2.rectangle(frame, (x_c_1, y_c_1), (x_c_2, y_c_2), (0, 255, 0), 3)
+    calibration_frame = frame[y_c_1:y_c_2, x_c_1:x_c_2]
+    cal_hsv = cv2.cvtColor(calibration_frame, cv2.COLOR_BGR2HSV)
+    x_pos = int(abs(x_c_1 - x_c_2)/2)
+    h_val = cal_hsv[:,:,0]
+    s_val = cal_hsv[:,:,1]
+    v_val = cal_hsv[:,:,2]
+    h_val.sort()
+    s_val.sort()
+    v_val.sort()
+    #discard outliers
+    (h,w) = h_val.shape
+    h_low = h//8
+    w_low = w//8
+    h_high = h-h_low
+    w_high = w-w_low
+    h_val_ab = h_val[h_low:h_high,w_low:w_high]
+    s_val_ab = s_val[h_low:h_high,w_low:w_high]
+    v_val_ab = v_val[h_low:h_high,w_low:w_high]
+    avg_h = np.average(h_val_ab)
+    avg_s = np.average(s_val_ab)
+    avg_v = np.average(v_val_ab)
+    hsv_avg = np.array([int(avg_h),int(avg_s),int(avg_v)])
+    lower_thresh_player = np.array([int(avg_h)-30,int(avg_s)-40,int(avg_v)-40])
+    upper_thresh_player = np.array([int(avg_h)+30,int(avg_s)+100,int(avg_v)+100])
+
+def calibration():
+    global counter
+    global lower_thresh_player
+    global upper_thresh_player
+    while (True):
+        ret, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        if counter <= 301:
+            get_calibration_frames(frame)
+        elif counter == 302:
+            print('calibrating...')
+            t.sleep(1)
+            calibrate(frame, x_c_1, y_c_1, x_c_2, y_c_2)
+        elif counter > 302:
+            print('exiting calibration...')
+            return
+        counter = counter+1
+        cv2.imshow('calibrating frame', frame)
+        # Display the resulting frames
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+print('center apron in green box')
+calibration()
+cv2.destroyAllWindows()
+
+while(True):
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    track_player(frame, lower_thresh_player, upper_thresh_player)
+    cv2.imshow('calibrating frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
