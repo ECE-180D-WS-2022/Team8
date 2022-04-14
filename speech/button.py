bd7b5ebc10ed175dd59e06e61d86666d9b52cab9
Jpower3 <jaustria2000@gmail.com>
2022-04-13 17:42:25 -0700

    second button trial

--- speech/button.py
+++ speech/button.py
@@ -8,17 +8,24 @@ from time import sleep
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	#Set pin 7 to be an input pin and set init. value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	#Set pin 10 to be an input pin and set init. value to be pulled low (off)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	#Same for pin 11
 
 length = 0
 flag = 0
+enter = 0
 
 
 while True:
-	if GPIO.input(10) == GPIO.HIGH and flag == 0:
-		flag = 1
-	if flag == 1:
-		print(length)
-		length += 1
-		sleep(2)
-		flag = 0
+	if GPIO.input(11) == GPIO.HIGH and enter == 0:
+		enter = 1
+		print('entered station')
+	while enter == 1:
+		if GPIO.input(10) == GPIO.HIGH and flag == 0:
+			flag = 1
+		if flag == 1:
+			print(length)
+			length += 1
+			sleep(2)
+			flag = 0
+			enter = 0
